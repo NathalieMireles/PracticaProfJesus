28690567e9102d7af5a3398ccb6425f1fc4cfbd7
NathalieMireles <natrodriguez628@gmail.com>
2024-09-28 22:47:24 -0600

    chamba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Card } from 'react-bootstrap'
+import { useState } from 'react';
 import './App.css';
+import { FormOne,FormTwo,FormThree,FormFour,FormFive,FormSix,FormSeven,FormEight,FormNine,FormTen } from './componentes/Forms';
 
 function App() {
+
+  const [data, setData] = useState({})
+
+  const onChange = (e) => {
+    e.preventDefault()
+    const obj = data
+    obj[e.target.name] = e.target.value
+    setData(obj)
+  }
+
+  const onClick = () => {
+    console.log(data)
+    alert('ok')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Card className='mt-3 '>
+          <FormOne onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormTwo onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormThree onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormFour onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormFive onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormSix onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card>
+          <FormSeven onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormEight onChange={onChange} onClick={onClick}/>
+        </Card>
+        <Card className='mt-3 '>
+          <FormNine onChange={onChange} onClick={onClick}/>
+        </Card> 
+        <Card className='mt-3 '>
+          <FormTen onChange={onChange} onClick={onClick}/>
+        </Card>
+      </Container>
+    </>
   );
 }
 
